43aa7d9b29402b5f656ce5951887f49d788a5d1a
chrispy <cpulicken1@gmail.com>
2017-10-31 21:07:30 -0500

    1st UI Push

--- RunningRhythm/AppLoginViewController.swift
+++ RunningRhythm/AppLoginViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreData
 
 class AppLoginViewController: UIViewController {
-
+    
     var loginList = [NSManagedObject]()
     
     @IBOutlet weak var usernameLabel: UILabel!
@@ -25,7 +25,7 @@ class AppLoginViewController: UIViewController {
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Login")
         do {
             loginList = try managedContext.fetch(fetchRequest)
-//            print(loginList)
+            //            print(loginList)
             self.usernameLabel.text = loginList[0].value(forKey: "username") as! String
         } catch let error as NSError {
             print("Could not fetch. \(error), \(error.userInfo)")
@@ -37,21 +37,21 @@ class AppLoginViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
