3c40974391227344fe3aa4c1b2261c1c9fc98396
Eucharist <ek7332@lib-pclem018.lib.utexas.edu>
2017-11-17 19:19:14 -0600

    Timer Update

--- RunningRhythm/HealthDataViewController.swift
+++ RunningRhythm/HealthDataViewController.swift
@@ -7,13 +7,49 @@
 //
 
 import UIKit
+import HealthKit
+import HealthKitUI
 
 class HealthDataViewController: UIViewController {
 
+    @IBOutlet weak var minute: UILabel!
+    @IBOutlet weak var second: UILabel!
+    public var timer: Timer!
+    var duration = 0
+    var minDuration = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        HKWorkoutEvent.self
+        
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func startWorkout(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateLabels), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pauseWorkout(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    @IBAction func EndWorkout(_ sender: UIButton) {
+        timer.invalidate()
+        duration = 0
+        minDuration = 0
+        second.text = String(duration)
+        minute.text = "\(minDuration):"
+    }
+    
+    func updateLabels() {
+        duration += 1
+        second.text = String(duration)
+        if (duration == 59) {
+            minDuration += 1
+            duration = 0
+        }
+        minute.text = "\(minDuration):"
     }
 
     override func didReceiveMemoryWarning() {
