7b10a0cdf8af711d245d45afe543c43ab6f1dea6
Eucharist <ek7332@lib-pclem018.lib.utexas.edu>
2017-11-17 19:41:12 -0600

    StepCount0.01

---
+++ RunningRhythm/HealthKitHelper.swift
@@ -0,0 +1,30 @@
+//
+//  HealthKitHelper.swift
+//  RunningRhythm
+//
+//  Created by Kun, Eucharist H on 11/17/17.
+//  Copyright © 2017 Cinder Capital. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import HealthKit
+
+class HealthKitManager {
+    
+    let healthStore = HKHealthStore()
+    func authorizeHealthKit() -> Bool {
+        var isEnabled = true
+        if HKHealthStore.isHealthDataAvailable() {
+            let stepsCount = NSSet(object: HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!)
+            healthStore.requestAuthorization(toShare: nil, read: (stepsCount as! Set<HKObjectType>)) {
+                (success, error) -> Void in
+                isEnabled = success
+                }
+        }
+        else {
+        isEnabled = false
+        }
+        return isEnabled
+        }
+}
